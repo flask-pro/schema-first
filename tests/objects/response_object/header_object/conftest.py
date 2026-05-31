4e87d7123758ff8bc503ca439f374c03280d5c99
Konstantin Fdeev <fadeev@stantis.ru>
2026-05-31 22:23:13 +0300

    add header schema.

--- tests/objects/response_object/header_object/conftest.py
+++ tests/objects/response_object/header_object/conftest.py
@@ -3,6 +3,7 @@ import pytest
 
 @pytest.fixture
 def fx_header_object_full(fx_parameter_object_from_path_full) -> dict:
-    fx_parameter_object_from_path_full.pop('name')
-    fx_parameter_object_from_path_full.pop('in')
-    return fx_parameter_object_from_path_full
+    header_object = {
+        k: v for k, v in fx_parameter_object_from_path_full.items() if k not in ['name', 'in']
+    }
+    return header_object
